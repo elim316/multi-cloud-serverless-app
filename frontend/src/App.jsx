087146aca4df05c5a1f8964df4981272b63307b0
elim316 <2200832@sit.singaporetech.edu.sg>
2025-09-11 03:12:41 +0800

    feat(frontend): added in supabase functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 export default function App() {
   const [message, setMessage] = useState("");
@@ -13,7 +11,7 @@ export default function App() {
   }
 
   return (
-    <div style={{ padding: 24}}>
+    <div style={{ padding: 24 }}>
       <h1>Multi-Cloud Serverless App</h1>
       <button onClick={callApi}>Call Backend</button>
       <p>{message}</p>
