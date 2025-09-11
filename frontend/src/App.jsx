e8288323f1a4597797e2ed1783b4430a4c3a61b8
elim316 <2200832@sit.singaporetech.edu.sg>
2025-09-12 00:03:34 +0800

    feat(api): add realtime supabase updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { supabase } from "./supabaseClient";
 
 export default function App() {
   const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   async function callApi() {
     const res = await fetch("/api/hello");
@@ -10,11 +12,61 @@ export default function App() {
     setMessage(text);
   }
 
-  return (
+  async function saveMessage() {
+    if (!message) return;
+    const { error } = await supabase
+      .from("messages")
+      .insert([{ text: message }]); // insert a row into supabase table
+    if (error) console.error(error);
+    setMessage(""); // clear input box
+    loadMessages(); // reload the list
+  }
+
+  async function loadMessages(params) {
+    const { data, error } = await supabase
+      .from("messages")
+      .select("*")
+      .order("id", { ascending: false }); // newest first
+    if (error) console.error(error);
+    setMessages(data); // update state
+  }
+
+  // Auto load messages on start
+  useEffect(() => {
+    loadMessages();
+
+    const channel = supabase
+      .channel("public:messages")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "messages"},
+        (payload) => {
+          console.log("New message:", payload.new);
+          setMessages((prev) => [payload.new, ...prev]);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+
+  }, []); // empy [] means it runs once and not on every render
+
+return (
     <div style={{ padding: 24 }}>
       <h1>Multi-Cloud Serverless App</h1>
-      <button onClick={callApi}>Call Backend</button>
-      <p>{message}</p>
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Write a message"
+      />
+      <button onClick={saveMessage}>Save</button>
+      <ul>
+        {messages.map((m) => (
+          <li key={m.id}>{m.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
